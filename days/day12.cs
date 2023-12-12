81a57982229805d6d04fd5062249b6b9515e95e2
RutgervO <rutger@nhtcu.nl>
2023-12-13 00:10:11 +0100

    day 12 fast

--- days/day12.cs
+++ days/day12.cs
@@ -1,5 +1,3 @@
-using AOC.util;
-
 namespace AOC.days;
 
 internal class Day12 : Day<long>
@@ -16,42 +14,111 @@ internal class Day12 : Day<long>
     {
         var inputValues = GetListOfLines(inputName);
         var unfolding = part * 4 - 3;
-        var mapping = new[] { '#', '.' };
         long result = 0;
 
         foreach (var parts in inputValues.Select(line => line.Split(' ').ToArray()))
         {
             var condition = string.Join('?', Enumerable.Repeat(parts[0], unfolding));
             var rules = string.Join(',', Enumerable.Repeat(parts[1], unfolding)).Split(',').Select(int.Parse).ToArray();
+            var pattern = string.Join('.', condition.Split('.', StringSplitOptions.RemoveEmptyEntries));
+            var cache = new Dictionary<string, long>();
+            
+            result += Solve(0, 0, 0);
+            continue;
 
-            var groupings = condition.Split('.', StringSplitOptions.RemoveEmptyEntries);
-            var groupingsString = string.Join('.', groupings);
-            var unknowns = groupingsString.Count(x => x == '?');
-            var options = Math.Pow(2, unknowns);
-            var positions = groupingsString.SelectWhere((_, i) => i, (c, _) => c == '?').ToArray();
-            for (long option = 0; option < options; option++)
+            long Solve(int pos, int count, int rule)
             {
-                var test = groupingsString.ToCharArray();
-                long bit = 1;
-                for (var pos = 0; pos < unknowns; pos++)
+                while (true)
                 {
-                    test[positions[pos]] = mapping[(option & bit) / bit];
-                    bit <<= 1;
-                }
+                    if (cache.TryGetValue(GetKey(), out var solve))
+                    {
+                        return solve;
+                    }
+                    
+                    if (rule == rules.Length)
+                    {
+                        return count > 0 ?
+                            // no rule for current grouping - impossible
+                            SaveResult(0) :
+                            // Can only fit in the rules if there are no '#'
+                            SaveResult(pattern[pos..].Contains('#') ? 0 : 1);
+                    }
 
-                if (new string(test)
-                    .Split('.', StringSplitOptions.RemoveEmptyEntries)
-                    .Where(x => x[0] == '#')
-                    .Select(x => x.Length)
-                    .ToArray()
-                    .SequenceEqual(rules))
-                {
-                    result += 1;
+                    if (count > rules[rule])
+                    {
+                        // No way to solve it
+                        return SaveResult(0);
+                    }
+
+                    if (pos == pattern.Length)
+                    {
+                        // ran out of input
+                        if ((count == 0 && rules.Length == rule)
+                            || (rules.Length - rule == 1 && rules[rule] == count))
+                        {
+                            // either remaining rules is empty and we are not in a grouping,
+                            // or current grouping matches the last remaining rule
+                            return SaveResult(1);
+                        }
+
+                        return SaveResult(0);
+                    }
+
+                    if (pattern[pos] == '#')
+                    {
+                        // has to be added to current grouping
+                        pos += 1;
+                        count += 1;
+                        continue;
+                    }
+
+                    if (pattern[pos] == '.')
+                    {
+                        if (count > 0)
+                        {
+                            // current grouping must end and be correct
+                            if (rules.Length == rule || count != rules[rule])
+                            {
+                                // incorrect
+                                return SaveResult(0);
+                            }
+
+                            rule += 1;
+                        }
+
+                        pos += 1;
+                        count = 0;
+                        continue;
+                    }
+
+                    // pattern[pos] == '?'
+                    // solve all possibilities
+                    long total = 0;
+                    
+                    if (rules.Length > rule && count < rules[rule])
+                    {   // try '#' if there are still rules and we're not exceeding the current rule
+                        total += Solve(pos + 1, count + 1, rule);
+                    }
+
+                    if (count == 0)
+                    {   // '.' while not in grouping
+                        total += Solve(pos + 1, count, rule);
+                    }
+                    else
+                    {
+                        if (rules.Length > rule && count == rules[rule])
+                        {   // '.' ending grouping
+                            total += Solve(pos + 1, 0, rule + 1);
+                        }
+                    }
+
+                    return SaveResult(total);
+
+                    string GetKey() => $"{pos},{count},{rule}";
+                    long SaveResult(long value) => cache[GetKey()] = value;
                 }
             }
         }
-
         return result;
-
     }
 }
